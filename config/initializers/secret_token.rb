e6dc62646fb1a541ef37caec14da8902477a2b7e
Ian White <ianwhite@utexas.edu>
2014-03-22 09:57:40 -0500

    Initial commit. No bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoreMyStuffApp::Application.config.secret_key_base = '3f60cfefebe6510e1fe360085a860ce6d4341446001853b2ec0c5f8fb5beca4b23d4baddec52ba5b4c3a33aeac04b48feabd7ebe652b7b3265de5a5851b655cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StoreMyStuffApp::Application.config.session_store :cookie_store, key: '_StoreMyStuffApp_session'
